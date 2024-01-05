075e16f3ae3569946291ca3c2680d9cb0c43add5
Metta Surendhar <msurendhar8815@gmail.com>
2024-01-06 02:11:39 +0530

    completed development

--- src/App.js
+++ src/App.js
@@ -1,92 +1,97 @@
 // Import dependencies
-import React, { useRef, useState, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as cocossd from "@tensorflow-models/coco-ssd";
-import Webcam from "react-webcam";
-import "./App.css";
-import { drawRect } from "./utilities";
+import React, { useRef, useState, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as cocossd from '@tensorflow-models/coco-ssd';
+import Webcam from 'react-webcam';
+import './App.css';
+import { drawRect } from './utilities';
 
 function App() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
+	const webcamRef = useRef(null);
+	const canvasRef = useRef(null);
 
-  // Main function
-  const runCoco = async () => {
-    const net = await cocossd.load();
-    console.log("Handpose model loaded.");
-    //  Loop and detect hands
-    setInterval(() => {
-      detect(net);
-    }, 10);
-  };
+	// Main function
+	const runCoco = async () => {
+		const net = await cocossd.load();
+		console.log('Handpose model loaded.');
+		//  Loop and detect hands
+		setInterval(() => {
+			detect(net);
+		}, 10);
+	};
 
-  const detect = async (net) => {
-    // Check data is available
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      // Get Video Properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+	const detect = async (net) => {
+		// Check data is available
+		if (
+			typeof webcamRef.current !== 'undefined' &&
+			webcamRef.current !== null &&
+			webcamRef.current.video.readyState === 4
+		) {
+			// Get Video Properties
+			const video = webcamRef.current.video;
+			const videoWidth = webcamRef.current.video.videoWidth;
+			const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+			// Set video width
+			webcamRef.current.video.width = videoWidth;
+			webcamRef.current.video.height = videoHeight;
 
-      // Set canvas height and width
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
+			// Set canvas height and width
+			canvasRef.current.width = videoWidth;
+			canvasRef.current.height = videoHeight;
 
-      // Make Detections
-      const obj = await net.detect(video);
+			// Make Detections
+			const obj = await net.detect(video);
+			// console.log(obj);
 
-      // Draw mesh
-      const ctx = canvasRef.current.getContext("2d");
-      drawRect(obj, ctx); 
-    }
-  };
+			// Draw mesh
+			const ctx = canvasRef.current.getContext('2d');
+			drawRect(obj, ctx);
+		}
+	};
 
-  useEffect(()=>{runCoco()},[]);
+	// useEffect(() => {
+	// 	runCoco();
+	// }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          muted={true} 
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+	runCoco();
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 8,
-            width: 640,
-            height: 480,
-          }}
-        />
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<Webcam
+					ref={webcamRef}
+					muted={true}
+					style={{
+						position: 'absolute',
+						marginLeft: 'auto',
+						marginRight: 'auto',
+						left: 0,
+						right: 0,
+						textAlign: 'center',
+						zIndex: 9,
+						width: 640,
+						height: 480,
+					}}
+				/>
+
+				<canvas
+					ref={canvasRef}
+					style={{
+						position: 'absolute',
+						marginLeft: 'auto',
+						marginRight: 'auto',
+						left: 0,
+						right: 0,
+						textAlign: 'center',
+						zIndex: 8,
+						width: 640,
+						height: 480,
+					}}
+				/>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/utilities.js
+++ src/utilities.js
@@ -1,21 +1,23 @@
-export const drawRect = (detections, ctx) =>{
-  // Loop through each prediction
-  detections.forEach(prediction => {
+export const drawRect = (detections, ctx) => {
+	// Loop through each prediction
+	detections.forEach((prediction) => {
+		// console.log(prediction);
+		// Extract boxes and classes
+		const [x, y, width, height] = prediction['bbox'];
+		const text = prediction['class'];
+		console.log(x, y, width, height, text);
 
-    // Extract boxes and classes
-    const [x, y, width, height] = prediction['bbox']; 
-    const text = prediction['class']; 
+		// Set styling
+		// const color = Math.floor(Math.random()*16777215).toString(16);
+		// const color = 'red';
+		ctx.strokeStyle = ctx.font = 'stop l';
 
-    // Set styling
-    const color = Math.floor(Math.random()*16777215).toString(16);
-    ctx.strokeStyle = '#' + color
-    ctx.font = '18px Arial';
-
-    // Draw rectangles and text
-    ctx.beginPath();   
-    ctx.fillStyle = '#' + color
-    ctx.fillText(text, x, y);
-    ctx.rect(x, y, width, height); 
-    ctx.stroke();
-  });
-}
+		// Draw rectangles and text
+		ctx.beginPath();
+		ctx.fillStyle = 'red';
+		ctx.fillText(text, x, y);
+		ctx.rect(x, y, width, height);
+		ctx.stroke();
+		ctx.fill();
+	});
+};
